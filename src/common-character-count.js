b65e9006bcbcbd8efa9c37ec621d9920a50ae02b
Yulia <linguavoice1@yahoo.com>
2024-12-28 04:55:49 +0300

    fix: common cgaracter count function implemnted

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,7 +12,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
+  // create frequdncy maps for both strings;
+   const count1 = {};
+   const count2 = {};
 
+   // populate the frequency map for the first string;
+   for (let char of s1) {
+     count1[char] = (count1[char] || 0) + 1;
+   }
+
+   // populate the frequency map for the second string;
+   for (let char of s2) {
+    count2[char] = (count2[char] || 0) + 1;
+   }
+
+   //calculate the number of common characters;
+   let commonCount = 0;
+   // iterate through the first frequency map
+   for (let char in count1) {
+     if (count2[char]) {
+      // add the min number of the common chars
+      commonCount += Math.min(count1[char], count2[char]);
+     }
+   }
+   return commonCount;
 }
 
 module.exports = {
