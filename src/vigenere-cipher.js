fda17766e041816436207191c8c845e680c6fcfa
Yulia <linguavoice1@yahoo.com>
2025-01-05 13:33:27 +0300

    fix: vigenere-cipher solved

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -29,61 +29,79 @@ const { NotImplementedError } = require('../extensions/index.js');
 // Use Case: Online media services that need to protect their content from piracy.
 // Email Encryption:
 
+// easy expanation:
+// Class: Think of a class as a blueprint for creating a special kind of machine.
+// Constructor: This is like the setup function that runs when we create a new machine. It sets up some basic rules:
+// direct: Decides if the machine gives the message in the normal order or reversed.
+// _alpha: This is the alphabet, all the letters from A to Z.
+// _errorMsg: This is the message we show if something goes wrong.
+
+
+// _converter: This is the secret worker inside the machine that does the actual work of changing the message.
+// Check for Errors: If there's no message or keyword, it shows an error.
+// Prepare Message and Key: It makes the message and key all uppercase and repeats the key to match the message length.
+// Loop Through Message: For each letter in the message:
+// If it's a letter (A-Z), it finds the position of the corresponding letter in the key.
+// It shifts the alphabet based on the key letter and either encrypts or decrypts the message letter.
+// If it's not a letter (like a space or punctuation), it just adds it to the result as is.
+// Return Result: It returns the final message, either in normal order or reversed.
+// Visual Representation of _converter
+// Encrypt and Decrypt Methods
+// ;
+// encrypt: This method uses the _converter to change the message using the keyword to make it secret.
+// decrypt: This method uses the _converter to change the secret message back to the original message using the keyword.
+
+// Message:  H  E  L  L  O  shift these letters message, keyword, false);
+// Keyword:  K  E  Y  K  E
+// Shift:   +10 +4 +24 +10 +4 counting from letter A
+// Result:   R  I  J  V  S
+// Message: The text you want to encrypt.
+// Keyword: The word used to determine the shift for each letter in the message.
+// Alphabet: The set of letters we use for shifting.
+
+// Explanation
+// Finding Indexes: The _alphabet string is used to find the index of each letter in the message and the keyword.
+// Creating Shifted Alphabets: By slicing and concatenating _alphabet, the code creates a shifted version of the alphabet based on the current letter of the keyword.
+// Encrypting/Decrypting: The shifted alphabet is then used to map each letter in the message to its encrypted or decrypted counterpart.
+
 
 class VigenereCipheringMachine {
-  //set the machine in the direct mode:
-  constructor(isDirect = true) {
-     this.isDirect = isDirect;
+  constructor(direct = true) {
+    this.direct = direct;
+    this._alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this._errorMessage = 'Incorrect arguments!';
   }
-  encrypt(message, key) {
-    // if (arguments.length < 2) throw new Error('Incorrect arguments');
-    if (!message || !key) throw new Error('Incorrect arguments');
-      message = message.toUpperCase();
-      key = key.toUpperCase();
-      let result = '';
-      let keyIndex = 0;
 
-      //Calculate Encrypted Character Code:
-      //Unicode values are numerical representations of characters, 
-      //making it easy to perform arithmetic operations
-      // iterate each character in the message
-      for (let i = 0; i < message.length; i++) {
-        if (message[i] >= 'A' && message[i] <= 'Z') {
-           const messageCharCode = message.charCodeAt(i);
-           // gets the Unicode value of the character at the current keyIndex in the key.
-           const keyCharCode = key.charCodeAt(keyIndex % key.length);
-           const encryptedCharCode = ((messageCharCode - 65 + keyCharCode - 65) % 26) + 65;
-           result += String.fromCharCode(encryptedCharCode);
-           keyIndex++;
-        } else {
-             result += message[i];
-        }
-      }
-      return this.isDirect ? result : result.split('').reverse().join('');
-  }
-  decrypt(message, key) {
-    if (!message || !key) throw new Error('Incorrect arguments');
-    message = message.toUpperCase();
-    key = key.toUpperCase();
-    let result = '';
-    let keyIndex = 0;
-
-    //iterate each character in the message
-    for (let i = 0; i < message.length; i++) {
-      if (message[i] >= 'A' && message[i] <= 'Z') {
-        const messageCharCode  = message.charCodeAt(i);
-        const keyCharCode = key.charCodeAt(keyIndex % key.length);
-        const decryptedCharCode = ((messageCharCode - 65 - (keyCharCode - 65) + 26) % 26) + 65;
-        result += String.fromCharCode(decryptedCharCode);
-        keyIndex++;
-      } else {
-        result += message[i];
-      }
+  _converter(message, keyword, encrypt = true) {
+    if (!message || !keyword) {
+      throw new Error(this._errorMessage);
     }
+        const msg = message.toUpperCase().split('');
+        const key = keyword.repeat(Math.ceil(message.length / keyword.length)).toUpperCase();
+        let i = 0;
 
-     return this.isDirect ? result : result.split(''). reverse().join('');
-  }
-}
+        const encrMsg = msg.reduce((resultMsg, currentLetter) => {
+          if (this._alphabet.includes(currentLetter)) {
+            const letterIndexInAlphabet  = this._alphabet.indexOf(key[i++]);
+            const newAlphabet = `${this._alphabet.slice(letterIndexInAlphabet)}${this._alphabet.slice(0, letterIndexInAlphabet)}`;
+            return encrypt ?
+                 `${resultMsg}${newAlphabet[this._alphabet.indexOf(currentLetter)]}` :
+                 `${resultMsg}${this._alphabet[newAlphabet.indexOf(currentLetter)]}`;
+          }  else {
+                return `${resultMsg}${currentLetter}`;
+          }
+        }, '');
+        return this.direct ? encrMsg : encrMsg.split('').reverse().join('');
+      }
+
+        encrypt(message, keyword) {
+           return this._converter(message, keyword);
+        }
+
+        decrypt(message, keyword) {
+          return this._converter(message, keyword, false);
+        }
+    }
 
 module.exports = {
   VigenereCipheringMachine
